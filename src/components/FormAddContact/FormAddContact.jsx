a585fde46d8ab817bbf599ba797826d543c55fcf
sunny7657 <ksvetlana466@gmail.com>
2023-12-18 20:03:37 +0100

    add src

---
+++ src/components/FormAddContact/FormAddContact.jsx
@@ -0,0 +1,58 @@
+import { Component } from 'react';
+import styled from 'styled-components';
+
+const BtnStyled = styled.button`
+  display: block;
+  margin-top: 20px;
+  font-size: 12px;
+  padding: 8px;
+`;
+
+export class FormAddContact extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  handleFormSubmit = evt => {
+    evt.preventDefault();
+    this.props.onSubmit(this.state);
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            required
+            placeholder="Adam Smith"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label>
+          Number
+          <input
+            type="tel"
+            name="number"
+            required
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
+            placeholder="000-00-00"
+            value={this.state.number}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <BtnStyled type="submit">Add contact</BtnStyled>
+      </form>
+    );
+  }
+}
